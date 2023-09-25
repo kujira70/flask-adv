00e071cdfd06f6fabb5352ebb756b17d23a91eaf
kujira70 <sajitaban@gmail.com>
2023-09-25 17:44:52 +0900

    from surface

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 
 app = Flask(__name__)
 
-# Load the story data from the Excel file
-df = pd.read_excel("story_data.xlsx")
+# Load the data from the Excel file
+df = pd.read_excel("story_data.xlsx", index_col="node_key")
 
 @app.route('/')
-def start():
-    return story("start")
+def index():
+    return redirect(url_for('story', node_key='start'))
 
-@app.route('/<node_key>')
+@app.route('/story/<node_key>', methods=['GET', 'POST'])
 def story(node_key):
-    row_num = df[df['node_key'] == node_key].index[0]
-    story_text = df.iloc[row_num]['text']
-    choices = {
-        'Q': df.iloc[row_num]['choice_q'],
-        'W': df.iloc[row_num]['choice_w'],
-        'E': df.iloc[row_num]['choice_e'],
-        'R': df.iloc[row_num]['choice_r']
-    }
-    next_nodes = {
-        'Q': df.iloc[row_num]['next_q'],
-        'W': df.iloc[row_num]['next_w'],
-        'E': df.iloc[row_num]['next_e'],
-        'R': df.iloc[row_num]['next_r']
-    }
-    return render_template("story.html", story_text=story_text, choices=choices, next_nodes=next_nodes)
+    if request.method == 'POST':
+        choice = request.form.get('choice')
+        if choice == '0':
+            node_key = df.loc[node_key, 'next_q']
+        elif choice == '1':
+            node_key = df.loc[node_key, 'next_w']
+        elif choice == '2':
+            node_key = df.loc[node_key, 'next_e']
+        elif choice == '3':
+            node_key = df.loc[node_key, 'next_r']
 
-if __name__ == "__main__":
+    story_text = df.loc[node_key, 'text']
+    choices = {
+        'q': df.loc[node_key, 'choice_q'],
+        'w': df.loc[node_key, 'choice_w'],
+        'e': df.loc[node_key, 'choice_e'],
+        'r': df.loc[node_key, 'choice_r']
+    }
+    return render_template('story.html', story=story_text, choices=choices)
+
+if __name__ == '__main__':
     app.run(debug=True)
